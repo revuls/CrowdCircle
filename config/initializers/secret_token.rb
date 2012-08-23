80901dda57e8897dc07bdb93946e21eb227fb4a3
revul <revuls@gmail.com>
2012-08-23 22:29:39 +0200

    First Commit for the Ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdcircle::Application.config.secret_token = '45e72ba49c93823df20909aef397749f686755e4875a04136af913a9234ad55a4a93f304da957c5b3b198bdef67d7e2bb49ac796ddedf9f5bafed5dc68acb4af'
